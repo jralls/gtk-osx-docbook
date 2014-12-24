55419344c9d82ed06f50bfe22a924c49ad59f68e
Ryan Lortie <desrt@desrt.ca>
2014-12-24 09:38:52 -0500

    tweak install.sh for jhbuild
    Make two fixes to install.sh.
    
    1) Do a proper DESTDIR install instead of bypassing DESTDIR and creating
       some empty directories to cause jhbuild to think that we did the
       right thing.  This means that 'jhbuild uninstall' will work properly.
    
    2) Install the xml catalogs in $prefix/share/xml because this is where
       gtk-doc will look for them.

--- install.sh
+++ install.sh
@@ -5,21 +5,7 @@ if [ "x$JHBUILD_PREFIX" = x ]; then
     exit 1
 fi
 
-for f in `find etc -type f ! -name *.png`; do
-    mkdir -p $JHBUILD_PREFIX/`dirname $f`
-    cat $f | sed -e "s,/usr/share,$JHBUILD_PREFIX/share,g" \
-                 -e "s,/etc,$JHBUILD_PREFIX/etc,g" > $JHBUILD_PREFIX/$f
-done
-
 for f in `find share -type f ! -name *.png`; do
-    mkdir -p $JHBUILD_PREFIX/`dirname $f`
-    cat $f | sed -e "s,/usr/share,$JHBUILD_PREFIX/share,g" \
-                 -e "s,/etc,$JHBUILD_PREFIX/etc,g" > $JHBUILD_PREFIX/$f
+    mkdir -p "$DESTDIR/$JHBUILD_PREFIX/`dirname $f`"
+    cat $f | sed -e "s,/usr/share,$JHBUILD_PREFIX/share,g" > "$DESTDIR/$JHBUILD_PREFIX/$f"
 done
-
-
-if [ -n $DESTDIR ]; then
-    mkdir -p "$DESTDIR/$JHBUILD_PREFIX/etc"
-    mkdir -p "$DESTDIR/$JHBUILD_PREFIX/share"
-fi
-
